f9a2f2cfaa1a607d9b53cbcba2610a40d5590d59
akashvyavhare <akashvyawhare73@gmail.com>
2023-09-13 16:11:02 +0530

    first commit

---
+++ src/main/java/com/dev1/Dev1Application.java
@@ -0,0 +1,21 @@
+package com.dev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Dev1Application {
+	
+	@GetMapping("/")
+	public String getMassage() {
+		return "devops...!!!";
+		
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Dev1Application.class, args);
+	}
+
+}
